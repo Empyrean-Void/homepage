8c91cd45075b47189b0e37be4696fdf1f4675f83
Empyrean-Void <jshortz@proton.me>
2023-05-27 17:57:23 -0600

    Updated colors and bookmarks

--- bookmarks.js
+++ bookmarks.js
@@ -1,36 +1,38 @@
 // Note: having length != 4 will mess with layout based on how the site is styled
 const bookmarks = [
-  {
-    title: "Daily",
-    links: [
-      { name: "Brightspace", url: "https://mycourses.cnm.edu/d2l/home" },
-      { name: "Email", url: "https://outlook.office365.com/mail/" },
-      { name: "GitHub", url: "https://github.com/Empyrean-Void" },
-    ],
-  },
-  {
-    title: "Linux",
-    links: [
-      { name: "Arch Wiki", url: "https://wiki.archlinux.org" },
-      { name: "EndeavourOS", url: "https://endeavouros.com" },
-    ],
-  },
-  {
-    title: "Media",
-    links: [
-      { name: "Youtube", url: "https://youtube.com" },
-      { name: "Youtube Music", url: "https://music.youtube.com" },
-    ],
-  },
-  {
-    title: "Reddit",
-    links: [
-      {
-        name: "/r/EndeavourOS",
-        url: "https://reddit.com/r/EndeavourOS",
-      },
-      { name: "/r/firefoxcss", url: "https://reddit.com/r/firefoxcss" },
-      { name: "/r/unixporn", url: "https://reddit.com/r/unixporn" },
-    ],
-  },
+    {
+        title: "Daily",
+        links: [
+          /*{ name: "Brightspace", url: "https://mycourses.cnm.edu/d2l/home" },*/
+            { name: "Email", url: "https://mail.proton.me/u/0/inbox"},
+            { name: "School Email", url: "https://outlook.office365.com/mail/" },
+            { name: "GitHub", url: "https://github.com/Empyrean-Void" },
+        ],
+    },
+
+    {
+        title: "Linux",
+        links: [
+            { name: "Arch Wiki", url: "https://wiki.archlinux.org" },
+            { name: "Awesome", url: "https://awesomewm.org/doc/api/" },
+            { name: "EndeavourOS", url: "https://endeavouros.com" },
+        ],
+    },
+
+    {
+        title: "Media",
+        links: [
+            { name: "Youtube", url: "https://youtube.com" },
+            { name: "Youtube Music", url: "https://music.youtube.com" },
+        ],
+    },
+
+    {
+        title: "Reddit",
+        links: [
+            { name: "r/awesome", url: "https://www.reddit.com/r/awesomewm/" },
+            { name: "/r/EndeavourOS", url: "https://reddit.com/r/EndeavourOS" },
+            { name: "/r/unixporn", url: "https://reddit.com/r/unixporn" },
+        ],
+    },
 ];
